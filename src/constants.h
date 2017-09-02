923ae00fbb36b4e44e702c81ed5bcdf58d9c1138
Michał Soida <msoida@users.noreply.github.com>
2017-09-02 17:23:37 +0200

    Basic code layout

---
+++ src/constants.h
@@ -0,0 +1,6 @@
+const char* ssid     = "SSID";
+const char* password = "PASS";
+const char* mqtt_server = "SERVER";
+
+const char* mqtt_client = "CLIENTID";
+const char* mqtt_status_topic = "STATUS_TOPIC";
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#include "constants.h"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void setup() {
+    // TODO
+}
+
+
+void loop() {
+    // TODO
+}
